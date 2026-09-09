f1aa77dd1344c7bb987e5c41c50fcb56f220a382
Rajas Puranik <rajastalk2u@gmail.com>
2026-09-09 22:24:31 +0530

    feat: add Android native project and APK packaging

---
+++ android/app/src/main/java/in/gov/kisantrack/MainActivity.java
@@ -0,0 +1,5 @@
+package in.gov.kisantrack;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.gov.kisantrack',
+  appName: 'KisanTrack',
+  webDir: 'dist'
+};
+
+export default config;
